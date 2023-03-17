5bb4e43bf0ded498ce493a5ddd1e3548e60e0727
Dave <davidgeorgebell1995@outlook.com>
2023-03-17 23:24:01 +0000

    Delete-button-functionality

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const textBox = document.querySelector(".text-field");
 //list selectors
 const listWrapper = document.querySelector(".list-container");
 const mainList = document.querySelector(".list-container");
-const mainListChildren = mainList.getElementsByTagName("div");
 
 //Listeners
 addButton.addEventListener("click", newListItem);
@@ -18,23 +17,33 @@ textBox.addEventListener("keydown", function (event) {
 /*Creates a new list item by creating a div and appending the innerHTML with a template literal. 
 conditional will check if there is content within the text box and append the listWrapper*/
 function newListItem() {
+  const d = crypto.randomUUID().split("-").splice(0, 3).join("");
+  const uniqueIdentifier = d.valueOf();
+
   const element = `
   <div class="item-menu">
-    <button class="menu-button delete-button"><span class="material-symbols-outlined"> close </span>
+    <button class="menu-button delete-button" id="DB-${uniqueIdentifier}"><span class="material-symbols-outlined"> close </span>
       </button>
     <button class="menu-button edit-button"><span class="material-symbols-outlined"> menu </span>
       </button>
   </div>
   <div class="list-content">
-    <p>${mainListChildren.length}. ${textBox.value}</p>
+    <p>${textBox.value}</p>
   </div>`;
 
-  let listItem = document.createElement("div");
+  const listItem = document.createElement("div");
+
   listItem.classList.add("list-item");
+  listItem.id = uniqueIdentifier;
   listItem.innerHTML = element;
 
   if (textBox.value) {
     listWrapper.appendChild(listItem);
+
+    const deleteButton = document.querySelector(`#DB-${uniqueIdentifier}`)
+    deleteButton.addEventListener('click', function (){
+      listItem.remove()
+    })
   }
 
   textBox.value = "";
